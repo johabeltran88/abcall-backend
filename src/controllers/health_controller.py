33849d90f701e03fd6f3dff38ddcad47b968f370
Alejandro Martinez <alejandro.martinez.az@gmail.com>
2024-10-03 11:23:56 -0500

    Commit inicial

--- src/controllers/health_controller.py
+++ src/controllers/health_controller.py
@@ -5,4 +5,4 @@ health_bp = Blueprint('health', __name__)
 
 @health_bp.route('/health', methods=['GET'])
 def health():
-    return make_response('Service up!', 200)
+    return make_response('Service up !', 200)
