ddd6e67f796c262c2e53b5be7fde9c51a1664912
Alejandro Martinez <alejandro.martinez.az@gmail.com>
2024-11-13 20:50:54 -0500

    Agregar servicio para la edicion de un PQR

--- src/controllers/pcc_controller.py
+++ src/controllers/pcc_controller.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_jwt_extended import get_jwt
 
 from src.commons.decorator import roles_required
-from src.services.client_service import ClientService
+from src.commons.validation_util import ValidationUtil
+from src.models.notification import Notification
 from src.services.pcc_service import PccService
 
 pcc_bp = Blueprint('pcc', __name__)
@@ -12,4 +13,14 @@ pcc_bp = Blueprint('pcc', __name__)
 @roles_required('AGENT', 'CONSUMER', 'CLIENT')
 def get_pcc_by_id(pcc_id):
     pcc = PccService.get_pcc_by_id(pcc_id, get_jwt().get("consumer_id"))
+    return jsonify(pcc.to_dict_with_consumer_and_company_and_notifications()), 200
+
+
+@pcc_bp.route('/pccs/<pcc_id>', methods=['PUT'])
+@roles_required('AGENT')
+def update_pcc(pcc_id):
+    ValidationUtil.validate_not_blank(request, 'status', 'reason')
+    ValidationUtil.validate_length(request, {'name': 'reason', 'min': 8, 'max': 1000})
+    notification = Notification(request.json['status'], request.json['reason'])
+    pcc = PccService.update_pcc(pcc_id, notification)
     return jsonify(pcc.to_dict_with_consumer_and_company_and_notifications()), 200
